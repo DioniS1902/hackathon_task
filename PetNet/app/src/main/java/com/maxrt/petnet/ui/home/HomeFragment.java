e3cda0fb3b38cc5db31ae02fd1bbdbb5b2731710
Maxym Tkachuk <s2m4f6@gmail.com>
2020-12-20 07:23:04 +0200

    Added json request method for retrieving sale info

--- PetNet/app/src/main/java/com/maxrt/petnet/ui/home/HomeFragment.java
+++ PetNet/app/src/main/java/com/maxrt/petnet/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.maxrt.petnet.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,24 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.maxrt.petnet.MainActivity;
 import com.maxrt.petnet.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RequestQueue queue;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,6 +38,36 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = root.findViewById(R.id.text_home);
 
+        queue = Volley.newRequestQueue(getContext());
+
+        getJSON();
+
         return root;
+    }
+
+    public void getJSON() {
+        String url = "https://jbokdxgfjkxfgnknl.000webhostapp.com/sales.php";
+        JsonObjectRequest request = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        setDataFields(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(request);
+    }
+
+    private void setDataFields(JSONObject json) {
+        try {
+            // Code
+        } catch (JSONException e) {
+            Log.e("HomeFragment", e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
